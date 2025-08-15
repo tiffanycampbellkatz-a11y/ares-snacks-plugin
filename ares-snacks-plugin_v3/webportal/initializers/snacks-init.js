60776c11856df6c5c346b4800902b81be0ce9e56
tiffanycampbellkatz-a11y <tiffanycampbellkatz@gmail.com>
2025-08-15 00:44:15 -0600

    Delete ares-snacks-plugin_v3 directory

--- ares-snacks-plugin_v3/webportal/initializers/snacks-init.js
+++
@@ -1,8 +0,0 @@
-
-export function initialize(application) {
-  // No-op: Ares core templates can include <ProfileSnacks /> via plugin autoload.
-  // This file ensures the component bundle is registered.
-}
-export default {
-  initialize
-};
